a71d2b3883baf5e0a0bc8eac279fd5a27a89909f
Sasha <prof.menko@gmail.com>
2023-12-06 20:00:56 +0100

    logical

--- 02 JavaScript Basic/Class02/Logical_Operators/script.js
+++ 02 JavaScript Basic/Class02/Logical_Operators/script.js
@@ -3,3 +3,33 @@ let score = 90;
 let hasPassed = !(score >= pass); // Negation
 console.log(hasPassed);
 console.log(!hasPassed);
+
+// Logical AND
+console.log("Logical AND");
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+// Logical OR
+console.log("Logical OR");
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+//Negation
+console.log("Negation");
+console.log(!true);
+console.log(!false);
+
+console.log("-------------------");
+let comparison1 = 5 < 10;
+let comparison2 = 10 != 10;
+let comparison3 = 10 !== "10";
+console.log(comparison1);
+console.log(comparison2);
+console.log(comparison3);
+console.log("-------------------");
+console.log(comparison1 && comparison2);
+console.log(!comparison2 || comparison3);
