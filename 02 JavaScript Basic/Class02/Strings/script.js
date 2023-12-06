e1fb4e5688c1d696862abf8a26aa75839eb73835
Sasha <prof.menko@gmail.com>
2023-12-06 18:31:01 +0100

    break

--- 02 JavaScript Basic/Class02/Strings/script.js
+++ 02 JavaScript Basic/Class02/Strings/script.js
@@ -1,1 +1,25 @@
 console.log("Hello" + " " + "SEDC!");
+console.log("Hello " + "SEDC!");
+
+//String interpolation
+let firstName = "John";
+console.log(`Hello ${firstName}!`);
+
+// alert(`Hello ${firstName}`);
+
+let first = 10;
+let second = "5";
+console.log("Conversion from number to string");
+let sumResult = first + second;
+console.log(first + second);
+console.log("The type is:", typeof sumResult);
+
+//Using single and double quotes
+let quote = "It's really nice to be a programmer.";
+console.log(quote);
+// let quote = 'It's really nice to be a programmer.'; Cannot use
+
+let newQuote = "It's really nice to be a programmer"; // 'It\'s really nice to be a programmer'
+console.log(newQuote);
+
+let guestMessage = 'The guest said "HELLO" to all of us.'; // "The guest said \"HELLO\" to all of us."
