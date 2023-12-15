d6f6f214dd1da8cdbe5f288b9229c8c804b18a05
Sasha <prof.menko@gmail.com>
2023-12-15 20:06:01 +0100

    Anonymus function in event

--- 02 JavaScript Basic/Class06/EventListeners/script.js
+++ 02 JavaScript Basic/Class06/EventListeners/script.js
@@ -14,7 +14,18 @@ function buttonClicked() {
 }
 
 function secondButtonClicked() {
-  alert("Button clicked, but the second one!");
+  alert("Button clicked, but from the second function!");
 }
 // myInputElement.addEventListener("focus", focusGreeting);
 // myInputElement.addEventListener("blur", blurGreeting);
+// myButton.addEventListener("click", buttonClicked);
+// myButton.addEventListener("click", secondButtonClicked);
+// myButton.addEventListener("click", function () {
+//   alert("Hello from our event listener in the annonymus function!");
+// });
+
+myButton.addEventListener("click", function () {
+  buttonClicked();
+  secondButtonClicked();
+  alert("Hello again after the two functions!");
+});
