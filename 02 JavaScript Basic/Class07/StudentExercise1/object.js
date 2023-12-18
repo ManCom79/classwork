349446e7fa807072a80d67d07a24a45c6956e412
Sasha <prof.menko@gmail.com>
2023-12-18 19:49:34 +0100

    edit object's properties

--- 02 JavaScript Basic/Class07/StudentExercise1/object.js
+++ 02 JavaScript Basic/Class07/StudentExercise1/object.js
@@ -15,19 +15,28 @@ let person = {
 console.log(person);
 console.log(person.character());
 
-//Second
-let person1 = new Object();
-person1.age = 44;
-person1.height = 179;
-person1.maritalStatus = "married";
-person1.employmentStatus = "employed";
-person1.happy = true;
+person.age = 25;
+person.heigth = 160;
+person.maritalStatus = "not married";
+person["employmentStatus"] = "not employed";
+person["happy"] = false;
 
-person1.character = function () {
-  console.log(
-    `The person in case is ${this.age} years old, is ${this.height} cm tall, is ${this.maritalStatus} and is ${this.employmentStatus}. It seems that this person is ${this.happy} hero.`
-  );
-};
+console.log(person);
+console.log(person.character());
 
-console.log(person1);
-console.log(person1.character());
+// //Second
+// let person1 = new Object();
+// person1.age = 44;
+// person1.height = 179;
+// person1.maritalStatus = "married";
+// person1.employmentStatus = "employed";
+// person1.happy = true;
+
+// person1.character = function () {
+//   console.log(
+//     `The person in case is ${this.age} years old, is ${this.height} cm tall, is ${this.maritalStatus} and is ${this.employmentStatus}. It seems that this person is ${this.happy} hero.`
+//   );
+// };
+
+// console.log(person1);
+// console.log(person1.character());
