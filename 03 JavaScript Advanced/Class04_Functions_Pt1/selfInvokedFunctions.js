d62f2b974215f02f72141e480a8bea9c0ea0c13d
Sasha <prof.menko@gmail.com>
2024-01-17 19:39:31 +0100

    SelfInvolved function

--- 03 JavaScript Advanced/Class04_Functions_Pt1/selfInvokedFunctions.js
+++ 03 JavaScript Advanced/Class04_Functions_Pt1/selfInvokedFunctions.js
@@ -5,3 +5,11 @@
     console.log(fruits[i]);
   }
 })();
+
+//SelfInvolved function using arrow function
+(() => {
+  let fruits = ["Apple", "Bananna", "Orange"];
+  for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+  }
+})();
