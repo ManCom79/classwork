2bec184fecb5d79075f99d69633ab38232a3d0e2
Stevan80 <150294279+Stevan80@users.noreply.github.com>
2024-01-24 19:35:05 +0100

    filter

--- 03 JavaScript Advanced/Class06_Functions_Pt2/higherOrderFunctions.js
+++ 03 JavaScript Advanced/Class06_Functions_Pt2/higherOrderFunctions.js
@@ -1,5 +1,13 @@
-const numbers = [2, 5, 8, 10, 15, 7, 3, 2, 12, 17, 34, 53];
+const numbers = [2, 5, 8, -10, 15, -7, 3, -2, 12, -17, 34, -53];
+const users = [{ id: 1, name: "Alice", age: 34 }];
 
-//map
+// //map
 const squaredNumbers = numbers.map((num) => num ** 2);
 console.log(squaredNumbers);
+
+//forEach
+numbers.forEach((num) => console.log(num));
+
+//filter
+const positiveNumbers = numbers.filter((num) => num > 0);
+console.log(positiveNumbers);
