d89d44db99c43636026a4a88f23ad8a3fd65dc18
Stevan80 <150294279+Stevan80@users.noreply.github.com>
2024-01-31 19:12:50 +0100

    Promise

--- Class09_Asynchronous_API_Call/script.js
+++ Class09_Asynchronous_API_Call/script.js
@@ -12,25 +12,46 @@ function fetchResource(url, successCallback, failureCallback) {
     .then((data) => successCallback(data))
     .catch((error) => failureCallback(error));
 }
+// COMMENTED FOR CLEAR CONSOLE FOR PROMISES
+// fetchResource(
+//   "https://jsonplaceholder.typicode.com/users",
+//   (users) => {
+//     console.log("Users found: ", users);
+//     fetchResource(
+//       "https://jsonplaceholder.typicode.com/todos",
+//       (todos) => {
+//         console.log("Todos found ", todos);
+//         fetchResource(
+//           "https://jsonplaceholder.typicode.com/posts",
+//           (posts) => {
+//             console.log("Posts found ", posts);
+//           },
+//           failureCallback
+//         );
+//       },
+//       failureCallback
+//     );
+//   },
+//   failureCallback
+// );
 
-fetchResource(
-  "https://jsonplaceholder.typicode.com/users",
-  (users) => {
-    console.log("Users found: ", users);
-    fetchResource(
-      "https://jsonplaceholder.typicode.com/todos",
-      (todos) => {
-        console.log("Todos found ", todos);
-        fetchResource(
-          "https://jsonplaceholder.typicode.com/posts",
-          (posts) => {
-            console.log("Posts found ", posts);
-          },
-          failureCallback
-        );
-      },
-      failureCallback
-    );
-  },
-  failureCallback
-);
+//PROMISES
+
+let randomNumberPromise = new Promise((resolve, reject) => {
+  const num = Math.random();
+  if (num >= 0.5) {
+    resolve("Promise is fullfiled!");
+  } else {
+    reject("Promise failed!");
+  }
+});
+
+function handleResolve(value) {
+  console.log(value);
+}
+
+function handleReject(reason) {
+  console.error(reason);
+}
+
+randomNumberPromise.then(handleResolve, handleReject);
